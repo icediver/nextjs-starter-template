069b825690a0b3a74347e37b5429bbc2e4f1179c
icediver <icedivet@live.ru>
2024-09-17 20:51:23 +0300

    fix some minor bags

--- src/components/layout/footer/Footer.tsx
+++ src/components/layout/footer/Footer.tsx
@@ -1,8 +1,9 @@
 import clsx from 'clsx';
+import Link from 'next/link';
+
 import Github from '@/assets/icons/github.svg';
 import Instagram from '@/assets/icons/instagram.svg';
 import Telegram from '@/assets/icons/telegram.svg';
-import Link from 'next/link';
 
 export function Footer() {
 	return (
@@ -14,17 +15,17 @@ export function Footer() {
 			<ul className="flex items-center gap-4">
 				<li>
 					<Link href="https://github.com/icediver">
-						<Github className="h-10 w-10 transition-all duration-300 hover:scale-125" />
+						<Github className="h-8 w-8 transition-all duration-300 hover:scale-125" />
 					</Link>
 				</li>
 				<li>
 					<Link href="https://www.instagram.com/1cediver/">
-						<Instagram className="h-12 w-16 transition-all duration-300 hover:scale-110 hover:brightness-200" />
+						<Instagram className="h-8 w-8 transition-all duration-300 hover:scale-125" />
 					</Link>
 				</li>
 				<li>
 					<Link href="https://t.me/ageev_as">
-						<Telegram className="h-12 w-16" />
+						<Telegram className="h-8 w-8 transition-all duration-300 hover:scale-125" />
 					</Link>
 				</li>
 			</ul>
--- src/components/ui/theme-switcher/ThemeSwitcher.tsx
+++ src/components/ui/theme-switcher/ThemeSwitcher.tsx
@@ -1,23 +1,23 @@
-"use client";
+'use client';
 
-import { Theme } from "./theme.type";
-import { useToggleTheme } from "./useToggleTheme";
-import Sun from "@/assets/icons/sun.svg";
-import Moon from "@/assets/icons/moon.svg";
+import { Theme } from './theme.type';
+import { useToggleTheme } from './useToggleTheme';
+import Sun from '@/assets/icons/sun.svg';
+import Moon from '@/assets/icons/moon.svg';
 
 interface IThemeSwitcher {
-  theme: Theme;
+	theme: Theme;
 }
 
 export default function ThemeSwitcher({ theme: currentTheme }: IThemeSwitcher) {
-  const { theme, toogleTheme } = useToggleTheme(currentTheme);
-  return (
-    <button onClick={toogleTheme} className="z-10">
-      {theme === Theme.dark ? (
-        <Sun className="h-8 w-8  fill-yellow-500" />
-      ) : (
-        <Moon className="h-7 w-7 [&>path]:fill-gray-300" />
-      )}
-    </button>
-  );
+	const { theme, toogleTheme } = useToggleTheme(currentTheme);
+	return (
+		<button onClick={toogleTheme} className="z-10">
+			{theme === Theme.dark ? (
+				<Sun className="h-8 w-8 fill-yellow-500" />
+			) : (
+				<Moon className="h-7 w-7 [&>path]:fill-gray-300" />
+			)}
+		</button>
+	);
 }
